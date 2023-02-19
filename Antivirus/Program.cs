4a0b9ddf7bb5c74bb144824a0d8d424f5dae8b64
g0ren <g0ren@mail.ru>
2023-02-19 15:57:05 +0200

    Add project files.

---
+++ Antivirus/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Antivirus
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] formNames = new string[] { 
+                "Form1", 
+                "DefenderForm0", 
+                "DefenderForm1", 
+                "DefenderForm2", 
+                "DefenderForm3", 
+                "DefenderForm4" };
+
+            /*
+             * Found classname by running:
+             * 
+             Console.WriteLine(WinAPI.GetWindowClassName(
+                WinAPI.FindWindowByWindowName("DefenderForm0"))
+                );
+             * It's the same for all the forms
+             */
+            string classname = "WindowsForms10.Window.8.app.0.141b42a_r7_ad1";
+
+            while (WinAPI.FindWindowByClassName(classname) > 0) { 
+                foreach (string formName in formNames)
+                {
+                    string name = formName;
+                    ThreadStart threadStart = new ThreadStart(() => {
+                        while (WinAPI.FindWindowByWindowName(name) > 0)
+                        {
+                            int hWnd = WinAPI.FindWindowByWindowName(name);
+                            WinAPI.SendQuitMessage(hWnd);
+                            // The Console.WriteLine instruction is not only useful for debugging,
+                            // but also acts like a little Sleep))
+                            Console.WriteLine($"Killed window {name} #{hWnd}"); 
+                        }
+                    });
+                    Thread thread = new Thread(threadStart);
+                    thread.Start();
+                }
+            }
+        }
+    }
+}
---
+++ Antivirus/WinAPI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ This class was blindly copied from previous HW. For all intents and purposes
+ it should be treated as a homebrew external library.
+ */
+
+
+namespace Antivirus
+{
+    internal class WinAPI
+    {
+        public static int SendQuitMessage(int hWnd)
+        {
+            return SendMessage(hWnd, 0x0010, 0, 0);
+        }
+
+        public static int FindWindowByWindowName(string windowName)
+        {
+            return FindWindow(null, windowName).ToInt32();
+        }
+
+        public static int FindWindowByClassName(string className)
+        {
+            return FindWindow(className, null).ToInt32();
+        }
+
+        public static string GetWindowClassName(int hWnd)
+        {
+            StringBuilder windowClassName = new StringBuilder(256);
+
+            GetClassName((IntPtr)hWnd, windowClassName, windowClassName.Capacity);
+
+            return windowClassName.ToString();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string? lpClassName, string? lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool SetWindowText(IntPtr hwnd, String lpString);
+    }
+}
